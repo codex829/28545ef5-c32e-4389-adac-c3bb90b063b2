827d390986df7fc14d9dae0d030accfe67f90485
Danu <dciptadi@gmail.com>
2024-05-03 06:13:28 +0700

    add mongoose package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee/employee.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/ambisius_interview_test'),
+    EmployeeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
